03241a3adb52a79c6b3154023eeab5fc60b93bef
mikesplore <mikepremium8@gmail.com>
2024-05-20 17:58:36 +0300

    upto here its fine

--- app/src/main/java/com/app/fitnessapp/Attendancerecording.kt
+++ app/src/main/java/com/app/fitnessapp/Attendancerecording.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons

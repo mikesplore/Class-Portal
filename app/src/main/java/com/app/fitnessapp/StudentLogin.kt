c5460d82e8b842ce2aeb4adaaa056454e719e768
mikesplore <mikepremium8@gmail.com>
2024-05-20 17:24:29 +0300

    upto here its fine

--- app/src/main/java/com/app/fitnessapp/StudentLogin.kt
+++ app/src/main/java/com/app/fitnessapp/StudentLogin.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.Button

3588c1cdf71f630150ce2a001a4141b3f52b2d96
mikesplore <mikepremium8@gmail.com>
2024-05-25 10:30:55 +0300

    initial commit

--- app/src/androidTest/java/com/app/classportal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/app/classportal/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.app.fitnessapp", appContext.packageName)
+        assertEquals("com.app.classportal", appContext.packageName)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Fitness APP"
+rootProject.name = "Class Portal"
 include(":app")

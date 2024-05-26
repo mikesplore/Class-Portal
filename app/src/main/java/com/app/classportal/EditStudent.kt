538542d4cc9ee0ab3dad82d7d88ff66b099ac5c9
mikesplore <mikepremium8@gmail.com>
2024-05-26 20:19:42 +0300

    redesigned the UI

--- app/src/main/java/com/app/classportal/EditStudent.kt
+++ app/src/main/java/com/app/classportal/EditStudent.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column

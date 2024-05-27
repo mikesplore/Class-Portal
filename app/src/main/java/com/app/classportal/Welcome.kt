22431968199199eaadf8dd1ba5930cb97d960863
mikesplore <mikepremium8@gmail.com>
2024-05-28 00:54:53 +0300

    Adding Color Pallete

--- app/src/main/java/com/app/classportal/Welcome.kt
+++ app/src/main/java/com/app/classportal/Welcome.kt
@@ -1,139 +1,132 @@
+
 package com.app.classportal
 
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-
-import android.content.Context
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.ScrollableTabRow
-import androidx.compose.material.Tab
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Transparent
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
-import com.app.classportal.ui.theme.RobotoMono
-import com.google.accompanist.pager.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 @Composable
-fun WelcomeScreen(navController: NavController) {
-    Box(modifier = Modifier
-        .background(primaryColor)
-        .fillMaxSize(),
+fun WelcomeScreen(navController: NavHostController) {
+    var startAnimation by remember { mutableStateOf(false) }
+    var fadeOut by remember { mutableStateOf(false) }
+    var showProgress by remember { mutableStateOf(false) } // State to show/hide progress
 
-        ) {
-        AsyncImage(
-            model = "https://i.imgur.com/FO3K7mW.jpeg",
-            contentDescription = "sample",
-            modifier = Modifier
-                .fillMaxSize(),
+    //Animation
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation && !fadeOut) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 2000,
+            easing = LinearEasing
+        ), label = ""
+    )
+    //Progress Value
+    val progress by animateFloatAsState(
+        targetValue = if (showProgress) 1f else 0f,
+        animationSpec = tween(6000), label = "Loading"
+    )
 
-            contentScale = ContentScale.Crop,
-            error = painterResource(id = R.drawable.error_image),
-            placeholder = painterResource(id = R.drawable.loading_image)
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(5000)
+        showProgress = true
+        delay(3000)
+        fadeOut = true
+        delay(2000)
+        navController.navigate("login") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(globalcolors.primaryColor),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "CLASS APP",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 60.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontFamily = FontFamily.Serif,
+                textAlign = TextAlign.Center,
+                shadow = Shadow(
+                    color = Color.Black.copy(alpha = 0.5f), // Shadow color (semi-transparent black)
+                    offset = Offset(4f, 4f), // Shadow offset (x, y)
+                    blurRadius = 8f // Shadow blur radius
+                )
+            ),
+            modifier = Modifier.alpha(alphaAnim.value)
         )
-        Column (modifier = Modifier
-
-            .fillMaxSize(),
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally)
-        {
+        val counter by derivedStateOf { (progress * 100).toInt() }
+        AnimatedVisibility(visible = showProgress) {
             Column(modifier = Modifier
-                .padding(bottom = 50.dp)
-                .fillMaxWidth(0.8f)
-                .height(150.dp),
-                verticalArrangement = Arrangement.SpaceBetween,
+                .height(250.dp),
+                verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally){
-            Button(onClick = { navController.navigate("login") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = tertiaryColor,
-                    contentColor = Color.Black
-                )) {
-                Text(text = "Login",
-                    fontFamily = RobotoMono,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
+                Box(contentAlignment = Alignment.Center){
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(70.dp),
+                        color = globalcolors.secondaryColor,
+                        strokeWidth = 5.dp,
+                        progress = progress
+                    )
+                    Text(
+                        text = "$counter%",
+                        style = myTextStyle,
+                        fontSize = 18.sp
+                    )
+
+                }
+
+                Text(
+                    text = when (counter) {
+                        0 -> "Loading..."
+                        in 1..20 -> "Please wait..."
+                        in 21..50 -> "Fetching data..."
+                        in 51..80 -> "Almost there..."
+                        else -> "Finishing up..."
+                    },
+                    style = myTextStyle,
+                    fontSize = 18.sp
                 )
 
             }
-            Button(onClick = { navController.navigate("login") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = tertiaryColor,
-                    contentColor = Color.Black
-                )) {
-                Text(text = "Sign Up",
-                    fontFamily = RobotoMono,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp
-                )
-
-            }
-        }}
 
 
+        }
 
     }
 }
 
 
+
 @Preview
 @Composable
-fun WelcomeScreenPreview() {
-    WelcomeScreen(rememberNavController())
+fun SplashScreenPreview() {
+    WelcomeScreen(navController = rememberNavController())
 }

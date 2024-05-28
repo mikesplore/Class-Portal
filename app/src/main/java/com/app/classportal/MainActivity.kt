cf4fc6568fe34081985842127f84e8b04d830002
mikesplore <mikepremium8@gmail.com>
2024-05-28 13:01:27 +0300

    Adding Color Pallete

--- app/src/main/java/com/app/classportal/MainActivity.kt
+++ app/src/main/java/com/app/classportal/MainActivity.kt
@@ -5,13 +5,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -27,6 +45,7 @@ class Global(
     var loggedinregID: MutableState<String> = mutableStateOf(""),
     var enableEdgeToEdge: MutableState<Boolean> = mutableStateOf(true),
     var usernames: MutableState<String> = mutableStateOf(""),
+    var showdialog: MutableState<Boolean> = mutableStateOf(false)
 
 
 
@@ -46,13 +65,14 @@ class MainActivity : ComponentActivity() {
             }
             val navController = rememberNavController()
             NavigationComponent(navController, this)
+            LoginDialog( navController = navController)
         }
     }
 
     @Composable
     fun NavigationComponent(navController: NavHostController, context: Context) {
         
-        NavHost(navController, startDestination = "welcome") {
+        NavHost(navController, startDestination = "announcements") {
             composable("dashboard") {
                 Dashboard(navController, context)
             }
@@ -113,9 +133,57 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+
+
     @Preview
     @Composable
     fun DefaultPreview() {
         NavigationComponent(rememberNavController(), this)
     }
+}
+
+
+@Composable
+fun LoginDialog(navController: NavController){
+    if(global.showdialog.value){
+        AlertDialog(
+            onDismissRequest = { global.showdialog.value = false},
+            confirmButton = {
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                Button(onClick = {
+                    global.showdialog.value = false
+                    navController.navigate("login")},
+                    colors = ButtonDefaults.buttonColors(globalcolors.primaryColor),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.width(130.dp)) {
+                    Text("Login now",
+                        style = myTextStyle,)
+                }
+                Button(onClick = {
+                    global.showdialog.value = false},
+                    colors = ButtonDefaults.buttonColors(globalcolors.primaryColor),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.width(130.dp)) {
+                    Text("Cancel",
+                        style = myTextStyle,)
+                }}
+
+            },
+            title = { Text("Login First",
+                style = myTextStyle,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold) },
+            text = { Text("You have to login to perform this action.",
+                style = myTextStyle,) },
+            containerColor = globalcolors.secondaryColor,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AutomaticLogoutPreview(){
+    LoginDialog(navController = rememberNavController())
 }

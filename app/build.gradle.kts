fc2cf5ba5b7211264dac293b18587b81432ba6f5
mikesplore <mikepremium8@gmail.com>
2024-05-28 15:27:51 +0300

    In-App notifications now available

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,11 +50,15 @@ android {
 }
 
 dependencies {
+
+    implementation ("androidx.core:core-ktx:1.6.0")
+    implementation ("androidx.compose.ui:ui:1.0.1")
+    implementation ("androidx.compose.material3:material3:1.0.0-alpha11")
+    implementation ("androidx.activity:activity-compose:1.3.1")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.accompanist.pager)
     implementation(libs.accompanist.pager.indicators)
     implementation(libs.androidx.work.runtime.ktx)
-    implementation(libs.material)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.coil.compose)
     implementation(libs.gson)

1c821a42c53e7871b6aa698eb1984c2e2869cee2
mikesplore <mikepremium8@gmail.com>
2024-05-31 20:21:13 +0300

    cleaning the app

--- app/src/main/java/com/app/classportal/CurrentDayLecturers.kt
+++ app/src/main/java/com/app/classportal/CurrentDayLecturers.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -16,14 +14,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.app.classportal.FileUtil.loadTimetable
 import com.app.classportal.ui.theme.RobotoMono
 import java.util.Calendar
@@ -36,7 +31,7 @@ fun CurrentDayEventsScreen() {
     val calendar = Calendar.getInstance()
     val dayOfWeek = (calendar.get(Calendar.DAY_OF_WEEK) + 5) % 7
     val timetableData = remember { loadTimetable(context) }
-    val lecturesToday = timetableData[dayOfWeek] ?: emptyList()
+    val lecturesToday = timetableData[dayOfWeek]
     val addbackbrush = remember {
         mutableStateOf(
             Brush.verticalGradient(
@@ -122,6 +117,6 @@ fun CurrentDayEventsScreen() {
 
 @Preview
 @Composable
- fun timetablePreview(){
+ fun CurrentEventPreview(){
      CurrentDayEventsScreen()
  }
